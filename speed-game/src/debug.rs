f37e88c33910f44b71de5e05a5c249614550011f
EggShark <ther3al3ggshark@gmail.com>
2024-04-07 22:16:41 -0500

    chrome book debug

--- speed-game/src/debug.rs
+++ speed-game/src/debug.rs
@@ -85,7 +85,7 @@ impl DebugText {
     }
 
     pub fn prepare(&mut self, engine: &mut Engine) {
-        if engine.is_key_pressed(Key::F3) {
+        if engine.is_key_pressed(Key::F3) || engine.is_key_down(Key::P) {
             self.active = !self.active;
         }
 
